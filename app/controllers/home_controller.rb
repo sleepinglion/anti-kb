92868857707529796c933418e8b5ff1743a94fb3
sleepinglion <toughjjh@gmail.com>
2017-06-05 11:44:29 +0900

    수정함:        app/controllers/home_controller.rb 	수정함:        app/controllers/users_controller.rb 	수정함:        app/views/users/index.html.erb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 
-class HomeController < ApplicationController 
-  
+class HomeController < ApplicationController
+
   def initialize
     super
     @style="home"
@@ -9,30 +9,29 @@ class HomeController < ApplicationController
   end
 
   def index
-    if(params[:tab])     
+    if(params[:tab])
       if(params[:tab]=='notice')
       @notices = Notice.order('id desc').page(0).per(12)
       end
-    
+
       if(params[:tab]=='article')
       @articles = Article.order('id desc').page(0).per(12)
-      end    
+      end
     else
       @users = User.order('id desc').page(params[:page]).per(12)
       @user_count=User.count
-      @user = User.new
     end
-    
+
     @models=Model.order('id desc').page(0).per(5)
     @reports = Report.order('id desc').page(0).per(5)
     @compliments = Compliment.order('id desc').page(0).per(5)
-    
+
   end
-  
+
   def kbsmind
     respond_to do |format|
       format.html { render layout: layout}
       format.json { render json: @faqContent  }
     end
   end
-end 
+end
