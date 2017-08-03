d4d14c8ef445c8246327b760c44fda6ef4425b54
sleepinglion <toughjjh@gmail.com>
2017-08-04 01:32:39 +0900

    수정함:        app/controllers/users/omniauth_callbacks_controller.rb 	수정함:        app/models/concerns/omniauth_attributes_concern.rb

--- app/models/concerns/omniauth_attributes_concern.rb
+++ app/models/concerns/omniauth_attributes_concern.rb
@@ -33,5 +33,16 @@ module OmniauthAttributesConcern
                     }
         create(attributes)
      end
+
+     def kakao params
+       (params['info']['email'] = "dummy#{SecureRandom.hex(10)}@dummy.com") if params['info']['email'].blank?
+       attributes = {
+                       email: params['info']['email'],
+                       name: params['info']['nickname'],
+                       password: Devise.friendly_token,
+                       description: '망해라'
+                   }
+       create(attributes)
+    end
      end
  end
