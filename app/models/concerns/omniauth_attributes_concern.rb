95ed984cb0783131c7bf478dd07928b35dffb24b
sleepinglion <toughjjh@gmail.com>
2017-08-14 23:41:41 +0900

    수정함:        app/controllers/users/omniauth_callbacks_controller.rb 	수정함:        app/models/concerns/omniauth_attributes_concern.rb 	수정함:        app/views/users/shared/_links.erb 	수정함:        config/initializers/devise.rb

--- app/models/concerns/omniauth_attributes_concern.rb
+++ app/models/concerns/omniauth_attributes_concern.rb
@@ -22,27 +22,5 @@ module OmniauthAttributesConcern
                      }
          create(attributes)
       end
-
-      def google_oauth2 params
-        (params['info']['email'] = "dummy#{SecureRandom.hex(10)}@dummy.com") if params['info']['email'].blank?
-        attributes = {
-                        email: params['info']['email'],
-                        name: params['info']['nickname'],
-                        password: Devise.friendly_token,
-                        description: '망해라'
-                    }
-        create(attributes)
-     end
-
-     def kakao params
-       (params['info']['email'] = "dummy#{SecureRandom.hex(10)}@dummy.com") if params['info']['email'].blank?
-       attributes = {
-                       email: params['info']['email'],
-                       name: params['info']['nickname'],
-                       password: Devise.friendly_token,
-                       description: '망해라'
-                   }
-       create(attributes)
-    end
      end
  end
